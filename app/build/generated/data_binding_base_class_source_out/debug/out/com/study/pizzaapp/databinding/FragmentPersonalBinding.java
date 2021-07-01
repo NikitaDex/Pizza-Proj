8238a21d89393e85f0470c6e23f0dbc82b561109
NikitaDex <demivakin@mail.ru>
2021-07-02 00:54:31 +0500

    Доработал Личный кабинет

--- app/build/generated/data_binding_base_class_source_out/debug/out/com/study/pizzaapp/databinding/FragmentPersonalBinding.java
+++ app/build/generated/data_binding_base_class_source_out/debug/out/com/study/pizzaapp/databinding/FragmentPersonalBinding.java
@@ -4,20 +4,90 @@ package com.study.pizzaapp.databinding;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.viewbinding.ViewBinding;
 import com.study.pizzaapp.R;
 import java.lang.NullPointerException;
 import java.lang.Override;
+import java.lang.String;
 
 public final class FragmentPersonalBinding implements ViewBinding {
   @NonNull
   private final FrameLayout rootView;
 
-  private FragmentPersonalBinding(@NonNull FrameLayout rootView) {
+  @NonNull
+  public final EditText EmailAddress;
+
+  @NonNull
+  public final EditText Number;
+
+  @NonNull
+  public final EditText address;
+
+  @NonNull
+  public final Button button4;
+
+  @NonNull
+  public final EditText card;
+
+  @NonNull
+  public final ImageView imageView2;
+
+  @NonNull
+  public final Button important;
+
+  @NonNull
+  public final Button myCupons;
+
+  @NonNull
+  public final Button myOrder;
+
+  @NonNull
+  public final TextView textView10;
+
+  @NonNull
+  public final TextView textView11;
+
+  @NonNull
+  public final TextView textView12;
+
+  @NonNull
+  public final TextView textView7;
+
+  @NonNull
+  public final TextView textView8;
+
+  @NonNull
+  public final TextView textView9;
+
+  private FragmentPersonalBinding(@NonNull FrameLayout rootView, @NonNull EditText EmailAddress,
+      @NonNull EditText Number, @NonNull EditText address, @NonNull Button button4,
+      @NonNull EditText card, @NonNull ImageView imageView2, @NonNull Button important,
+      @NonNull Button myCupons, @NonNull Button myOrder, @NonNull TextView textView10,
+      @NonNull TextView textView11, @NonNull TextView textView12, @NonNull TextView textView7,
+      @NonNull TextView textView8, @NonNull TextView textView9) {
     this.rootView = rootView;
+    this.EmailAddress = EmailAddress;
+    this.Number = Number;
+    this.address = address;
+    this.button4 = button4;
+    this.card = card;
+    this.imageView2 = imageView2;
+    this.important = important;
+    this.myCupons = myCupons;
+    this.myOrder = myOrder;
+    this.textView10 = textView10;
+    this.textView11 = textView11;
+    this.textView12 = textView12;
+    this.textView7 = textView7;
+    this.textView8 = textView8;
+    this.textView9 = textView9;
   }
 
   @Override
@@ -43,10 +113,105 @@ public final class FragmentPersonalBinding implements ViewBinding {
 
   @NonNull
   public static FragmentPersonalBinding bind(@NonNull View rootView) {
-    if (rootView == null) {
-      throw new NullPointerException("rootView");
-    }
+    // The body of this method is generated in a way you would not otherwise write.
+    // This is done to optimize the compiled bytecode for size and performance.
+    int id;
+    missingId: {
+      id = R.id.EmailAddress;
+      EditText EmailAddress = rootView.findViewById(id);
+      if (EmailAddress == null) {
+        break missingId;
+      }
 
-    return new FragmentPersonalBinding((FrameLayout) rootView);
+      id = R.id.Number;
+      EditText Number = rootView.findViewById(id);
+      if (Number == null) {
+        break missingId;
+      }
+
+      id = R.id.address;
+      EditText address = rootView.findViewById(id);
+      if (address == null) {
+        break missingId;
+      }
+
+      id = R.id.button4;
+      Button button4 = rootView.findViewById(id);
+      if (button4 == null) {
+        break missingId;
+      }
+
+      id = R.id.card;
+      EditText card = rootView.findViewById(id);
+      if (card == null) {
+        break missingId;
+      }
+
+      id = R.id.imageView2;
+      ImageView imageView2 = rootView.findViewById(id);
+      if (imageView2 == null) {
+        break missingId;
+      }
+
+      id = R.id.important;
+      Button important = rootView.findViewById(id);
+      if (important == null) {
+        break missingId;
+      }
+
+      id = R.id.my_cupons;
+      Button myCupons = rootView.findViewById(id);
+      if (myCupons == null) {
+        break missingId;
+      }
+
+      id = R.id.my_order;
+      Button myOrder = rootView.findViewById(id);
+      if (myOrder == null) {
+        break missingId;
+      }
+
+      id = R.id.textView10;
+      TextView textView10 = rootView.findViewById(id);
+      if (textView10 == null) {
+        break missingId;
+      }
+
+      id = R.id.textView11;
+      TextView textView11 = rootView.findViewById(id);
+      if (textView11 == null) {
+        break missingId;
+      }
+
+      id = R.id.textView12;
+      TextView textView12 = rootView.findViewById(id);
+      if (textView12 == null) {
+        break missingId;
+      }
+
+      id = R.id.textView7;
+      TextView textView7 = rootView.findViewById(id);
+      if (textView7 == null) {
+        break missingId;
+      }
+
+      id = R.id.textView8;
+      TextView textView8 = rootView.findViewById(id);
+      if (textView8 == null) {
+        break missingId;
+      }
+
+      id = R.id.textView9;
+      TextView textView9 = rootView.findViewById(id);
+      if (textView9 == null) {
+        break missingId;
+      }
+
+      return new FragmentPersonalBinding((FrameLayout) rootView, EmailAddress, Number, address,
+          button4, card, imageView2, important, myCupons, myOrder, textView10, textView11,
+          textView12, textView7, textView8, textView9);
+    }
+    String missingId = rootView.getResources().getResourceName(id);
+    throw new NullPointerException("Missing required view with ID: ".concat(missingId));
   }
 }
