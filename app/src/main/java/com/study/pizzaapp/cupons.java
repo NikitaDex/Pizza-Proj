9aa90626b43970cf27da4eb9b9218876ce32b943
Korobova <corobovaab@gmail.com>
2021-07-03 13:13:42 +0500

    Проблема со списком

--- app/src/main/java/com/study/pizzaapp/cupons.java
+++ app/src/main/java/com/study/pizzaapp/cupons.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +15,8 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class cupons extends Fragment {
+
+    String[]cupons={""}; // объявляю массив с купонами, остальное в криэйт
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,6 +29,7 @@ public class cupons extends Fragment {
 
     public cupons() {
         // Required empty public constructor
+
     }
 
     /**
@@ -44,11 +48,13 @@ public class cupons extends Fragment {
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
+
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -58,6 +64,9 @@ public class cupons extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_cupons, container, false);
     }
