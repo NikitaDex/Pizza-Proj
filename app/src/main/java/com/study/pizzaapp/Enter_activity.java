aad3252902f9f2a54dd74194d1fa3818cc424841
maxym <maxym.nagorny.60@gmail.com>
2021-07-06 15:14:30 +0500

    Сохранение данных пользователя на устройстве

--- app/src/main/java/com/study/pizzaapp/Enter_activity.java
+++ app/src/main/java/com/study/pizzaapp/Enter_activity.java
@@ -5,13 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class Enter_activity extends AppCompatActivity {
+
+    EditText mail;
+    EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter);
+        mail = (EditText)findViewById(R.id.username1);
+        password = (EditText)findViewById(R.id.password1);
     }
 
     public void btn_reg(View view) {
@@ -21,7 +27,12 @@ public class Enter_activity extends AppCompatActivity {
 
 
     public void enter_btn(View view) {
-        Intent Enter = new Intent("com.study.pizzaapp.MainActivity");
-        startActivity(Enter);
+        Authorization user = Authorization.Load(getApplicationContext());
+        //System.out.println(user.getMail() + " " + user.getPassword());
+        if (mail.getText().toString().equals(user.getMail()) &
+                password.getText().toString().equals(user.getPassword())) {
+            Intent Enter = new Intent("com.study.pizzaapp.MainActivity");
+            startActivity(Enter);
+        }
     }
 }
--- app/src/main/java/com/study/pizzaapp/RegisterWindow.java
+++ app/src/main/java/com/study/pizzaapp/RegisterWindow.java
@@ -6,14 +6,60 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import android.widget.EditText;
+
+
 import static com.study.pizzaapp.R.layout.activity_register_window;
 
 public class RegisterWindow extends AppCompatActivity {
+    EditText mail;
+    EditText pass;
+    EditText repeatPass;
+    EditText name;
+    EditText birthday;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(activity_register_window);
+        mail = (EditText)findViewById(R.id.mail_reg);
+        pass = (EditText)findViewById(R.id.pass_reg);
+        repeatPass = (EditText)findViewById(R.id.pass_reg_again);
+        name = (EditText)findViewById(R.id.name_reg);
+        birthday = (EditText)findViewById(R.id.wasBorn);
+    }
+    public boolean allFieldsFilled() {
+        if (mail.getText().toString().length() != 0 &
+                pass.getText().toString().length() != 0 &
+                repeatPass.getText().toString().length() != 0 &
+                name.getText().toString().length() != 0 &
+                birthday.getText().toString().length() != 0) {
+            return true;
+        }
+        return false;
+    }
+    public boolean rightPassword() {
+        if (pass.getText().toString().equals(repeatPass.getText().toString())) {
+            return true;
+        }
+        return false;
+    }
+    public void OnRegButtonClick(View view) {
+        System.out.println(allFieldsFilled());
+        System.out.println(rightPassword());
+        if (allFieldsFilled() & rightPassword()) {
+            Authorization user = new Authorization(getApplicationContext(),
+                    name.getText().toString(),
+                    pass.getText().toString(),
+                    mail.getText().toString(),
+                    "-",
+                    "-",
+                    "-",
+                    birthday.getText().toString());
+            user.Save();
+            Intent MainActivity = new Intent("com.study.pizzaapp.ZakazActivity");
+            startActivity(MainActivity);
+        }
     }
 
     public void register_btn(View view) {
