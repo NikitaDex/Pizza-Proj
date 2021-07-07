6037c5076546ed871bd4c81db9467ba913d1cbdb
PesBrodyaga <valera.nazargulov@mail.ru>
2021-07-07 11:34:50 +0500

    Внес изменения в методы работы с БД

--- app/src/main/java/com/study/pizzaapp/DBHelper.java
+++ app/src/main/java/com/study/pizzaapp/DBHelper.java
@@ -18,7 +18,7 @@ public class DBHelper extends SQLiteOpenHelper {
 
     private static String DB_NAME = "Пиццерия.db";
     private static String DB_PATH = "";
-    private static final int DB_VERSION = 5;
+    private static final int DB_VERSION = 7;
 
     private SQLiteDatabase mDataBase;
     private final Context mContext;
